4eea565f007ce40937fe98c8e77bb72c88bdbd16
Sami Daniel <samidanielpersonal@gmail.com>
2026-05-18 22:55:53 -0300

    Update Exercicio01.java

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -1,3 +1,6 @@
+// Nome: Sami Daniel Santos Silva
+// Matricula: 1261950200
+
 public class Exercicio01 {
     public static void imprimirArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
